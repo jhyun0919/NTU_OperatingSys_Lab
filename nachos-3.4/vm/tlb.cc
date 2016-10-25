d22cc1c11095cb3729144999601a864a78042fbd
Park Jee Hyun <jhyun19@gmail.com>
2016-10-25 11:46:51 +0800

    lab3
    virtual memory

--- nachos-3.4/vm/tlb.cc
+++ nachos-3.4/vm/tlb.cc
@@ -12,27 +12,26 @@
 // Figures out what to do (get from IPT, or pageoutpagein) and does it.
 //----------------------------------------------------------------------
 
-void UpdateTLB(int possible_badVAddr)
-{
-  int badVAddr;
-  unsigned int vpn;
-  int phyPage;
+void UpdateTLB(int possible_badVAddr) {
+    int badVAddr;
+    unsigned int vpn;
+    int phyPage;
 
-  if(possible_badVAddr) // get the bad address from the correct location
-    badVAddr=possible_badVAddr; // fault in kernel
-  else
-    badVAddr=machine->registers[BadVAddrReg]; // fault in userprog
-
-  vpn=(unsigned)badVAddr/PageSize;
-
-  if((phyPage=VpnToPhyPage(vpn))!=-1)
-    InsertToTLB(vpn, phyPage);
-  else {
-    if(vpn>=currentThread->space->numPages && !GetMmap(vpn))
-      machine->RaiseException(AddressErrorException, badVAddr);      
+    if (possible_badVAddr) // get the bad address from the correct location
+        badVAddr = possible_badVAddr; // fault in kernel
     else
-      InsertToTLB(vpn, PageOutPageIn(vpn));
- }
+        badVAddr = machine->registers[BadVAddrReg]; // fault in userprog
+
+    vpn = (unsigned) badVAddr / PageSize;
+
+    if ((phyPage = VpnToPhyPage(vpn)) != -1)
+        InsertToTLB(vpn, phyPage);
+    else {
+        if (vpn >= currentThread->space->numPages && !GetMmap(vpn))
+            machine->RaiseException(AddressErrorException, badVAddr);
+        else
+            InsertToTLB(vpn, PageOutPageIn(vpn));
+    }
 }
 
 //----------------------------------------------------------------------
@@ -40,21 +39,20 @@ void UpdateTLB(int possible_badVAddr)
 //      Gets a phyPage for a vpn, if exists in ipt.
 //----------------------------------------------------------------------
 
-int VpnToPhyPage(int vpn)
-{
-  //your code here to get a physical frame for page vpn
-  //you can refer to PageOutPageIn(int vpn) to see how an entry was created in ipt
+int VpnToPhyPage(int vpn) {
+    //your code here to get a physical frame for page vpn
+    //you can refer to PageOutPageIn(int vpn) to see how an entry was created in ipt
 
-  //Implementing the hash function to retrieve the list of entries
-  IptEntry* iptPtr = hashIPT(vpn, currentThread->pid);
+    //Implementing the hash function to retrieve the list of entries
+    IptEntry *iptPtr = hashIPT(vpn, currentThread->pid);
 
-  //while traversing to the next node in the linked list
-  while(iptPtr = iptPtr->next){
-	//if the node matches, return the physical page
-  	if(iptPtr->vPage == vpn && iptPtr->pid == currentThread->pid)
-  		return iptPtr->phyPage;
-  }
-	return -1;
+    //while traversing to the next node in the linked list
+    while (iptPtr = iptPtr->next) {
+        //if the node matches, return the physical page
+        if (iptPtr->vPage == vpn && iptPtr->pid == currentThread->pid)
+            return iptPtr->phyPage;
+    }
+    return -1;
 
 }
 
@@ -64,50 +62,49 @@ int VpnToPhyPage(int vpn)
 //	replacement
 //----------------------------------------------------------------------
 
-void InsertToTLB(int vpn, int phyPage)
-{
-  int i = 0; //entry in the TLB
+void InsertToTLB(int vpn, int phyPage) {
+    int i = 0; //entry in the TLB
 
-  //your code to find an empty in TLB or to replace the oldest entry if TLB is full
-  
-  static int FIFOPointer = 0;
+    //your code to find an empty in TLB or to replace the oldest entry if TLB is full
 
-  //Traverse through the TLB and proceed if there are invalid entry.
-  while(i<TLBSize){
-  	if(!machine->tlb[i].valid){
-		break;
-	}
-	i++;
-  }
+    static int FIFOPointer = 0;
 
-  //After traversing the TLB, if i equals to TLBSize, set i to the entry which pointed by the FIFOPointer.
-  if(i==TLBSize){
-	i = FIFOPointer;
-  }
+    //Traverse through the TLB and proceed if there are invalid entry.
+    while (i < TLBSize) {
+        if (!machine->tlb[i].valid) {
+            break;
+        }
+        i++;
+    }
 
-  //Move the FIFOPointer to the next entry.
-  FIFOPointer = (i+1)%TLBSize;
-  
-  // copy dirty data to memoryTable
-  if(machine->tlb[i].valid){
-    memoryTable[machine->tlb[i].physicalPage].dirty=machine->tlb[i].dirty;
-    memoryTable[machine->tlb[i].physicalPage].TLBentry=-1;
-  }
+    //After traversing the TLB, if i equals to TLBSize, set i to the entry which pointed by the FIFOPointer.
+    if (i == TLBSize) {
+        i = FIFOPointer;
+    }
 
-  //update the TLB entry
-  machine->tlb[i].virtualPage  = vpn;
-  machine->tlb[i].physicalPage = phyPage;
-  machine->tlb[i].valid        = TRUE;
-  machine->tlb[i].readOnly     = FALSE;
-  machine->tlb[i].use          = FALSE;
-  machine->tlb[i].dirty        = memoryTable[phyPage].dirty;
+    //Move the FIFOPointer to the next entry.
+    FIFOPointer = (i + 1) % TLBSize;
 
-  //update the corresponding memoryTable
-  memoryTable[phyPage].TLBentry=i;
-  DEBUG('p', "The corresponding TLBentry for Page %i in TLB is %i ", vpn, i);
-  //reset clockCounter to 0 since it is being used at this moment.
-  //for the implementation of Clock algorithm.
-  memoryTable[phyPage].clockCounter=0; 
+    // copy dirty data to memoryTable
+    if (machine->tlb[i].valid) {
+        memoryTable[machine->tlb[i].physicalPage].dirty = machine->tlb[i].dirty;
+        memoryTable[machine->tlb[i].physicalPage].TLBentry = -1;
+    }
+
+    //update the TLB entry
+    machine->tlb[i].virtualPage = vpn;
+    machine->tlb[i].physicalPage = phyPage;
+    machine->tlb[i].valid = TRUE;
+    machine->tlb[i].readOnly = FALSE;
+    machine->tlb[i].use = FALSE;
+    machine->tlb[i].dirty = memoryTable[phyPage].dirty;
+
+    //update the corresponding memoryTable
+    memoryTable[phyPage].TLBentry = i;
+    DEBUG('p', "The corresponding TLBentry for Page %i in TLB is %i ", vpn, i);
+    //reset clockCounter to 0 since it is being used at this moment.
+    //for the implementation of Clock algorithm.
+    memoryTable[phyPage].clockCounter = 0;
 }
 
 //----------------------------------------------------------------------
@@ -116,38 +113,37 @@ void InsertToTLB(int vpn, int phyPage)
 // bookkeeping. Use clock algorithm to find the replacement page.
 //----------------------------------------------------------------------
 
-int PageOutPageIn(int vpn)
-{
-  int phyPage; 
-  IptEntry *iptPtr; 
+int PageOutPageIn(int vpn) {
+    int phyPage;
+    IptEntry *iptPtr;
 
-  //increase the number of page faults
-  stats->numPageFaults++;
-  //call the clock algorithm, which returns the freed physical frame
-  phyPage=clockAlgorithm();
-  //Page out the victim page to free the physical frame
-  DoPageOut(phyPage);
-  //Page in the new page to the freed physical frame
-  DoPageIn(vpn, phyPage);
+    //increase the number of page faults
+    stats->numPageFaults++;
+    //call the clock algorithm, which returns the freed physical frame
+    phyPage = clockAlgorithm();
+    //Page out the victim page to free the physical frame
+    DoPageOut(phyPage);
+    //Page in the new page to the freed physical frame
+    DoPageIn(vpn, phyPage);
 
-  //make an entry in ipt
-  iptPtr=hashIPT(vpn, currentThread->pid);
-  while(iptPtr->next) iptPtr=iptPtr->next;
-  iptPtr->next=new IptEntry(vpn, phyPage, iptPtr);
-  iptPtr=iptPtr->next;
-  
-  //update memoryTable for this frame
-  memoryTable[phyPage].valid=TRUE;
-  memoryTable[phyPage].pid=currentThread->pid;
-  memoryTable[phyPage].vPage=vpn;
-  memoryTable[phyPage].corrIptPtr=iptPtr;
-  memoryTable[phyPage].dirty=FALSE;
-  memoryTable[phyPage].TLBentry=-1;
-  memoryTable[phyPage].clockCounter=0;
-  memoryTable[phyPage].swapPtr=currentThread->space->swapPtr;
+    //make an entry in ipt
+    iptPtr = hashIPT(vpn, currentThread->pid);
+    while (iptPtr->next) iptPtr = iptPtr->next;
+    iptPtr->next = new IptEntry(vpn, phyPage, iptPtr);
+    iptPtr = iptPtr->next;
 
- 
-  return phyPage;
+    //update memoryTable for this frame
+    memoryTable[phyPage].valid = TRUE;
+    memoryTable[phyPage].pid = currentThread->pid;
+    memoryTable[phyPage].vPage = vpn;
+    memoryTable[phyPage].corrIptPtr = iptPtr;
+    memoryTable[phyPage].dirty = FALSE;
+    memoryTable[phyPage].TLBentry = -1;
+    memoryTable[phyPage].clockCounter = 0;
+    memoryTable[phyPage].swapPtr = currentThread->space->swapPtr;
+
+
+    return phyPage;
 }
 
 //----------------------------------------------------------------------
@@ -155,42 +151,41 @@ int PageOutPageIn(int vpn)
 //      Actually pages out a phyPage to it's swapfile.
 //----------------------------------------------------------------------
 
-void DoPageOut(int phyPage)
-{
-  MmapEntry *mmapPtr;
-  int numBytesWritten;
-  int mmapBytesToWrite;
+void DoPageOut(int phyPage) {
+    MmapEntry *mmapPtr;
+    int numBytesWritten;
+    int mmapBytesToWrite;
 
-  if(memoryTable[phyPage].valid){           // check if pageOut possible
-    if(memoryTable[phyPage].TLBentry!=-1){
-      memoryTable[phyPage].dirty=
-	machine->tlb[memoryTable[phyPage].TLBentry].dirty;
-      machine->tlb[memoryTable[phyPage].TLBentry].valid=FALSE;
+    if (memoryTable[phyPage].valid) {           // check if pageOut possible
+        if (memoryTable[phyPage].TLBentry != -1) {
+            memoryTable[phyPage].dirty =
+                    machine->tlb[memoryTable[phyPage].TLBentry].dirty;
+            machine->tlb[memoryTable[phyPage].TLBentry].valid = FALSE;
+        }
+        if (memoryTable[phyPage].dirty) {        // pageOut is necessary
+            if ((mmapPtr = GetMmap(memoryTable[phyPage].vPage))) { // it's mmaped
+                DEBUG('p', "mmap paging out: pid %i, phyPage %i, vpn %i\n",
+                      memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
+                if (memoryTable[phyPage].vPage == mmapPtr->endPage)
+                    mmapBytesToWrite = mmapPtr->lastPageLength;
+                else
+                    mmapBytesToWrite = PageSize;
+                numBytesWritten = mmapPtr->openFile->
+                        WriteAt(machine->mainMemory + phyPage * PageSize, mmapBytesToWrite,
+                                (memoryTable[phyPage].vPage - mmapPtr->beginPage) * PageSize);
+                ASSERT(mmapBytesToWrite == numBytesWritten);
+            } else { // it's not mmaped
+                DEBUG('p', "paging out: pid %i, phyPage %i, vpn %i\n",
+                      memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
+                numBytesWritten = memoryTable[phyPage].swapPtr->
+                        WriteAt(machine->mainMemory + phyPage * PageSize, PageSize,
+                                memoryTable[phyPage].vPage * PageSize);
+                ASSERT(PageSize == numBytesWritten);
+            }
+        }
+        delete memoryTable[phyPage].corrIptPtr;
+        memoryTable[phyPage].valid = FALSE;
     }
-    if(memoryTable[phyPage].dirty){        // pageOut is necessary
-      if((mmapPtr=GetMmap(memoryTable[phyPage].vPage))){ // it's mmaped
-	DEBUG('p', "mmap paging out: pid %i, phyPage %i, vpn %i\n",
-	      memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
-	if(memoryTable[phyPage].vPage==mmapPtr->endPage)
-	  mmapBytesToWrite=mmapPtr->lastPageLength;
-	else
-	  mmapBytesToWrite=PageSize;
-	numBytesWritten=mmapPtr->openFile->
-	  WriteAt(machine->mainMemory+phyPage*PageSize, mmapBytesToWrite,
-		  (memoryTable[phyPage].vPage-mmapPtr->beginPage)*PageSize);
-	ASSERT(mmapBytesToWrite==numBytesWritten);
-      } else { // it's not mmaped
-	DEBUG('p', "paging out: pid %i, phyPage %i, vpn %i\n",
-	      memoryTable[phyPage].pid, phyPage, memoryTable[phyPage].vPage);
-	numBytesWritten=memoryTable[phyPage].swapPtr->
-	  WriteAt(machine->mainMemory+phyPage*PageSize, PageSize,
-		  memoryTable[phyPage].vPage*PageSize);
-	ASSERT(PageSize==numBytesWritten);
-      }
-    }
-    delete memoryTable[phyPage].corrIptPtr;
-    memoryTable[phyPage].valid=FALSE;
-  }
 }
 
 //----------------------------------------------------------------------
@@ -198,33 +193,32 @@ void DoPageOut(int phyPage)
 //      Actually pages in a phyPage/vpn combo from the swapfile.
 //----------------------------------------------------------------------
 
-void DoPageIn(int vpn, int phyPage)
-{
-  MmapEntry *mmapPtr;
-  int numBytesRead;
-  int mmapBytesToRead;
+void DoPageIn(int vpn, int phyPage) {
+    MmapEntry *mmapPtr;
+    int numBytesRead;
+    int mmapBytesToRead;
 
-  if((mmapPtr=GetMmap(vpn))){ // mmaped file
-    DEBUG('p', "mmap paging in: pid %i, phyPage %i, vpn %i\n",
-	  currentThread->pid, phyPage, vpn);
-    if(vpn==mmapPtr->endPage)
-      mmapBytesToRead=mmapPtr->lastPageLength;
-    else
-      mmapBytesToRead=PageSize;
-    numBytesRead=
-      mmapPtr->openFile->ReadAt(machine->mainMemory+phyPage*PageSize,
-				mmapBytesToRead,
-				(vpn-mmapPtr->beginPage)*PageSize);
-  ASSERT(numBytesRead==mmapBytesToRead);
-  } else { // not mmaped
-    DEBUG('p', "paging in: pid %i, phyPage %i, vpn %i\n", currentThread->pid,
-	  phyPage, vpn);
-    numBytesRead=currentThread->space->swapPtr->ReadAt(machine->mainMemory+
-							  phyPage*PageSize,
-							  PageSize,
-							  vpn*PageSize);
-  ASSERT(PageSize==numBytesRead);
-  }
+    if ((mmapPtr = GetMmap(vpn))) { // mmaped file
+        DEBUG('p', "mmap paging in: pid %i, phyPage %i, vpn %i\n",
+              currentThread->pid, phyPage, vpn);
+        if (vpn == mmapPtr->endPage)
+            mmapBytesToRead = mmapPtr->lastPageLength;
+        else
+            mmapBytesToRead = PageSize;
+        numBytesRead =
+                mmapPtr->openFile->ReadAt(machine->mainMemory + phyPage * PageSize,
+                                          mmapBytesToRead,
+                                          (vpn - mmapPtr->beginPage) * PageSize);
+        ASSERT(numBytesRead == mmapBytesToRead);
+    } else { // not mmaped
+        DEBUG('p', "paging in: pid %i, phyPage %i, vpn %i\n", currentThread->pid,
+              phyPage, vpn);
+        numBytesRead = currentThread->space->swapPtr->ReadAt(machine->mainMemory +
+                                                             phyPage * PageSize,
+                                                             PageSize,
+                                                             vpn * PageSize);
+        ASSERT(PageSize == numBytesRead);
+    }
 }
 
 //----------------------------------------------------------------------
@@ -234,38 +228,37 @@ void DoPageIn(int vpn, int phyPage)
 // discussed in the lectures.
 //----------------------------------------------------------------------
 
-int clockAlgorithm(void)
-{
-  int phyPage;
+int clockAlgorithm(void) {
+    int phyPage;
 
-  //your code here to find the physical frame that should be freed 
-  //according to the clock algorithm.
+    //your code here to find the physical frame that should be freed
+    //according to the clock algorithm.
 
-  static int clockPointer = 0;
+    static int clockPointer = 0;
 
-  while(true){
-  	printf("********************clock\n");
-	if(!memoryTable[clockPointer].valid)
-		break;
-	if(!memoryTable[clockPointer].dirty && memoryTable[clockPointer].clockCounter==OLD_ENOUGH)
-		break;
-	if(memoryTable[clockPointer].dirty && memoryTable[clockPointer].clockCounter==OLD_ENOUGH+DIRTY_ALLOWANCE)
-		break;
+    while (true) {
+        printf("********************clock\n");
+        if (!memoryTable[clockPointer].valid)
+            break;
+        if (!memoryTable[clockPointer].dirty && memoryTable[clockPointer].clockCounter == OLD_ENOUGH)
+            break;
+        if (memoryTable[clockPointer].dirty && memoryTable[clockPointer].clockCounter == OLD_ENOUGH + DIRTY_ALLOWANCE)
+            break;
 
-	//increase the clockCounter of an entry
-	memoryTable[clockPointer].clockCounter++;
+        //increase the clockCounter of an entry
+        memoryTable[clockPointer].clockCounter++;
 
-	//proceed to the next entry
-	clockPointer=(clockPointer+1)%NumPhysPages;
-  }
-  //if any of the above 3 if conditions are met, then physical should be freed
-  phyPage=clockPointer;
-  printf("Phypage = %d ", phyPage);
+        //proceed to the next entry
+        clockPointer = (clockPointer + 1) % NumPhysPages;
+    }
+    //if any of the above 3 if conditions are met, then physical should be freed
+    phyPage = clockPointer;
+    printf("Phypage = %d ", phyPage);
 
-  //increment the clockPointer
-  clockPointer=(clockPointer+1)%NumPhysPages; 
+    //increment the clockPointer
+    clockPointer = (clockPointer + 1) % NumPhysPages;
 
-  return phyPage;
+    return phyPage;
 }
 
 //----------------------------------------------------------------------
@@ -274,17 +267,16 @@ int clockAlgorithm(void)
 // 0 if does not exist.
 //----------------------------------------------------------------------
 
-MmapEntry *GetMmap(int vpn)
-{
-  MmapEntry *mmapPtr;
+MmapEntry *GetMmap(int vpn) {
+    MmapEntry *mmapPtr;
 
-  mmapPtr=currentThread->space->mmapEntries;
-  while(mmapPtr->next){
-    mmapPtr=mmapPtr->next;
-    if(vpn>=mmapPtr->beginPage && vpn<=mmapPtr->endPage)
-      return mmapPtr;
-  }
-  return 0;
+    mmapPtr = currentThread->space->mmapEntries;
+    while (mmapPtr->next) {
+        mmapPtr = mmapPtr->next;
+        if (vpn >= mmapPtr->beginPage && vpn <= mmapPtr->endPage)
+            return mmapPtr;
+    }
+    return 0;
 }
 
 //----------------------------------------------------------------------
@@ -293,14 +285,13 @@ MmapEntry *GetMmap(int vpn)
 // endPage.
 //----------------------------------------------------------------------
 
-void PageOutMmapSpace(int beginPage, int endPage)
-{
-  int vpn;
-  int phyPage;
+void PageOutMmapSpace(int beginPage, int endPage) {
+    int vpn;
+    int phyPage;
 
-  for(vpn=beginPage; vpn<=endPage; vpn++){
-    if((phyPage=VpnToPhyPage(vpn))==-1)
-      continue;
-    DoPageOut(phyPage);
-  }
+    for (vpn = beginPage; vpn <= endPage; vpn++) {
+        if ((phyPage = VpnToPhyPage(vpn)) == -1)
+            continue;
+        DoPageOut(phyPage);
+    }
 }
